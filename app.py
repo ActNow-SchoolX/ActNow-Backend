857dccadce8503fb3e54c3e33a5c66bb0830c6aa
Nikita Davidchuk <bizy18588@gmail.com>
2023-03-13 04:53:45 +0300

    add `on_startup` event for create all orm in database

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from src.backend.routes import nickname_validation
 from src.backend.routes import auth
 from src.backend.routes import user_registration
+from src.backend.database import engine
+from sqlmodel import SQLModel
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    SQLModel.metadata.create_all(engine)
+
+
 app.include_router(nickname_validation.app)
 app.include_router(auth.app)
 app.include_router(user_registration.app)
