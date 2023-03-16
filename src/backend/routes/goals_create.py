66c03ac5e8f2e4c6c428cb64a195ec6765f619eb
abakarova5665 <kabakarova23@gmail.com>
2023-03-16 20:04:42 +0300

    create goals

--- src/backend/routes/goals_create.py
+++ src/backend/routes/goals_create.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from typing import Optional
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from src.backend.database import engine
 
 app = FastAPI()
-engine = create_engine("sqlite:///mydatabase.db")
 Base = declarative_base()
 
 
