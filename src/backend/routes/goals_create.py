369e4fd14814aa58043f96440d9559192a68dc6b
abakarova5665 <kabakarova23@gmail.com>
2023-03-16 18:25:23 +0300

    ALG

---
+++ src/backend/routes/goals_create.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from typing import Optional
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+app = FastAPI()
+engine = create_engine("sqlite:///mydatabase.db")
+Base = declarative_base()
+
+
+class GoalsEntity(Base):
+    __tablename__ = "goal"
+    goal_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    price = Optional[Column(int)]
+    date = Optional[Column(int)]
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+@app.post("/goal_create")
+async def create_entity(name: str, description: str, price: int, date: int):
+    entity = GoalsEntity(name=name, description=description, cost=price, date=date)
+    session.add(entity)
+    session.commit()
+    return {"message": "Entity created successfully"}
