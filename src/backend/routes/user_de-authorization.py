62e5750e81a6c6cfef1719d7ed8c174953acd2d6
Евгений <dedaireks@gmail.com>
2023-03-17 01:01:56 +0300

    clean-up code

--- src/backend/routes/user_de-authorization.py
+++ src/backend/routes/user_de-authorization.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, Depends
-from fastapi.openapi.models import Response
-
-from src.backend.dependencies import backend, cookie
+import fastapi.openapi.models
+import src.backend.dependencies
 
 app = FastAPI()
 
 
 @app.post("/logout")
-async def logout(response: Response, session_uuid: Depends(cookie)):
-    await backend.delete(session_uuid)
-    cookie.delete_from_response(response)
+async def logout(response: fastapi.openapi.models.Response, session_uuid: Depends(src.backend.dependencies.cookie)):
+    await src.backend.dependencies.backend.delete(session_uuid)
+    src.backend.dependencies.cookie.delete_from_response(response)
     return {"message": "Logged out successfully"}
