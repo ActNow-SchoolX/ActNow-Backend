df56586c1fc7d9eb5b4cc50dfc863c99d14864d6
Влада <114161226+Vlada1356277@users.noreply.github.com>
2023-03-17 21:00:27 +0300

    Update nickname_validation.py

--- src/backend/routes/nickname_validation.py
+++ src/backend/routes/nickname_validation.py
@@ -3,14 +3,12 @@ from pydantic import BaseModel
 from src.backend.database.orm import User
 from sqlmodel import Session, SQLModel, select
 from src.backend.database import engine
-# from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse
 
 app = APIRouter()
 
-
 class Nicknames(BaseModel):
     nickname: str
-
 
 # логика типа
 def validate_nickname(value, users):
@@ -19,13 +17,10 @@ def validate_nickname(value, users):
             return False
     return True
 
-
 @app.post("/validate_nickname")
 def get_nickname(request: Nicknames):
     user1 = User(nickname="user1", password='password1')
     user2 = User(nickname="user2", password='password2')
-
-
 
     with Session(engine) as session:
         user1.create(session)
@@ -33,11 +28,16 @@ def get_nickname(request: Nicknames):
 
         statement = select(User)
         users = session.exec(statement)
-        # print(users)
 
     value = request.nickname
     if validate_nickname(value, users):
-        r = {"nickname": value, "available": True}
+        return JSONResponse(
+            status_code=200,
+            content={"nickname": value, "available": True},
+        )
     else:
-        r = {"nickname": value, "available": False}
-    return r
+        return JSONResponse(
+            status_code=409,
+            content={"nickname": value, "available": False},
+        )
+
