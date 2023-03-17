d7aaa69f5df462734cce36275043a5e18ffc50ba
Sergey <deusagile@gmail.com>
2023-03-17 21:57:47 +0300

    add goals_endpoint

--- src/backend/routes/goals_endpoint.py
+++ src/backend/routes/goals_endpoint.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from src.backend.dependencies import cookie, backend, verifier
 from sqlmodel import Session
 
@@ -11,25 +11,22 @@ from src.backend.database.orm import User
 
 app = APIRouter()
 
+
 @app.post('/goal', response_model=GoalResponse, dependencies=[Depends(cookie)])
-async def create_goal(item: GoalRequest, session: SessionData = Depends(verifier)):
+async def create_goal(response: Response, item: GoalRequest, session: SessionData = Depends(verifier)):
 
     with Session(engine) as ass:
 
         new_goal = goal_create(item, session.user_id)
 
         if new_goal is not None:
-            raise HTTPException(
-                status_code=201,
-                detail="Голс успешно создан!"
-            )
-        
-        return GoalResponse(id=new_goal.id,
-                            user_id=new_goal.user_id,
-                            title=new_goal.title,
-                            description=new_goal.description,
-                            price=new_goal.price,
-                            deadline=new_goal.deadline)
+            response.status_code = status.HTTP_201_CREATED
+            return GoalResponse(id=new_goal.id,
+                                user_id=new_goal.user_id,
+                                title=new_goal.title,
+                                description=new_goal.description,
+                                price=new_goal.price,
+                                deadline=new_goal.deadline)
         
 
     
