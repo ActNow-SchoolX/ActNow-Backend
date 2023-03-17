c857deace50b2c928edd6de7aa701c1a94c4c309
abakarova5665 <kabakarova23@gmail.com>
2023-03-17 20:25:51 +0300

    validate

--- src/backend/routes/goals.py
+++ src/backend/routes/goals.py
@@ -2,13 +2,24 @@ from __future__ import annotations
 from sqlmodel import Session, SQLModel
 from src.backend.database import engine
 from src.backend.database.orm import Goal
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class GoalRequest(BaseModel):
+    user_id: str
     title: str
     description: str
     price: float | None
+    deadline: str | None
+
+
+class GoalResponse(BaseModel):
+    id: int
+    user_id: int
+    title: str
+    description: str
+    price: float
+    deadline: str
 
 
 def goal_create(goal_data, user_id) -> Goal:
@@ -16,18 +27,27 @@ def goal_create(goal_data, user_id) -> Goal:
         title=goal_data.title,
         description=goal_data.description,
         price=goal_data.price,
-        user_id=user_id
+        user_id=user_id,
+        deadline=goal_data.deadline
     )
-
 
     with Session(engine) as transaction:
         Goal.create(goal, transaction)
 
+
+@validator('title')
+def check_title(cls, title):
+    if len(title) > 79:
+        raise ValueError('Название голса превышает возможное количество символов')
+    return title
+
+
+@validator('description')
+def check_title(cls, description):
+    if len(description) > 1000:
+        raise ValueError('Описание голса превышает возможное количество символов')
+    return description
+
     return goal
-
-
-
-
-
 
 
