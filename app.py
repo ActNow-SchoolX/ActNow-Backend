f8283e55d5aed24d5156f631c9245f64b60eacf3
Sergey <deusagile@gmail.com>
2023-03-17 19:31:39 +0300

    add app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.backend.routes import nickname_validation
 from src.backend.routes import auth
 from src.backend.routes import user_registration
+from src.backend.routes import goals_endpoint
 from src.backend.database import engine
 from sqlmodel import SQLModel
 
@@ -16,3 +17,4 @@ async def startup():
 app.include_router(nickname_validation.app)
 app.include_router(auth.app)
 app.include_router(user_registration.app)
+app.include_router(goals_endpoint.app)
