bc28e31e9d71f960db3be397eb64cf18a1cb8316
Nikita Davidchuk <bizy18588@gmail.com>
2023-03-18 12:35:17 +0300

    edit database connection and dockerfile

--- src/backend/database/__init__.py
+++ src/backend/database/__init__.py
@@ -1,9 +1,15 @@
 from os import environ
 from sqlmodel import create_engine
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(
-    environ.get("DATABASE_URL"),
-    echo=False,
-    connect_args=connect_args,
-)
+if environ.get('DATABASE_URL').startswith('mysql'):
+    engine = create_engine(
+        environ.get("DATABASE_URL"),
+        echo=False,
+    )
+elif environ.get('DATABASE_URL').startswith('sqlite'):
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(
+        environ.get("DATABASE_URL"),
+        echo=False,
+        connect_args=connect_args,
+    )
