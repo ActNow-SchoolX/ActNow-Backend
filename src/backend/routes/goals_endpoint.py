1ebb6506f8262e1207a26218cc5a97cc95bdf87c
Sergey <deusagile@gmail.com>
2023-03-22 22:22:02 +0300

    add goals_endpoint, delete get_goals

--- src/backend/routes/goals_endpoint.py
+++ src/backend/routes/goals_endpoint.py
@@ -3,6 +3,10 @@ from fastapi import APIRouter, Depends
 from src.backend.dependencies import cookie, verifier
 from src.backend.internals.goals import GoalRequest, GoalResponse, goal_create
 from src.backend.sessions import SessionData
+from src.backend.database.orm import Goal
+
+from sqlmodel import Session
+from src.backend.database import engine
 
 app = APIRouter()
 
@@ -13,3 +17,12 @@ async def create_goal(item: GoalRequest, session: SessionData = Depends(verifier
     new_goal = goal_create(item, session.user_id)
 
     return new_goal
+
+
+@app.get("/get_goal/{goal.id}", dependencies=[Depends(cookie)])
+def get_goal_by_id(goal_id: int) -> Goal:
+
+    with Session(engine) as transaction:
+        goal = Goal.get_by_id(transaction, goal_id)
+
+    return goal
