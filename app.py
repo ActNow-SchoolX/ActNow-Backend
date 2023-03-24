1a9c60ff8bdd2d469866a26e22c8e1f3d6acf847
Osadchuk Egor <gorlum1@outlook.com>
2023-03-24 15:58:28 +0300

    В этом коде мы получаем текущего пользователя и историю по story_id. Если пользователь или история не найдены, мы возбуждаем исключение HTTPException со статус-кодом 404. Затем мы проверяем, есть ли история в списке liked_stories пользователя, и если есть, мы удаляем ее, иначе добавляем. Затем мы сохраняем изменения в базе данных и возвращаем статус success и список liked_stories пользователя.

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from src.backend.routes import (
     stories_endpoint,
     users_endpoint,
     goals_endpoint,
-    deauthentication
+    deauthentication,
+    likes
 )
 
 app = FastAPI()
@@ -41,5 +42,10 @@ app.include_router(
 )
 app.include_router(
     stories_endpoint.app,
+    tags=["Story"]
+)
+
+app.include_router(
+    likes.app,
     tags=["Story"]
 )
--- src/backend/routes/likes.py
+++ src/backend/routes/likes.py
@@ -1,56 +1,33 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session
-from src.backend.database import get_session
-from src.backend.sessions import FastAPISession
-from src.backend.internals import add_like, remove_like, get_like_status
 
-router = APIRouter()
+from src.backend.database.orm import User
+from src.backend.dependencies import cookie, verifier
+from src.backend.sessions import SessionData
 
+app = APIRouter()
 
-# эндпойнт для добавления лайка
-@router.post("/likes/{item_id}")
-async def like_item(
-        item_id: int,
-        session: FastAPISession = Depends(),
-        db: Session = Depends(get_session),
+@app.post('/like-story/{story_id}', dependencies=[Depends(cookie)], status_code=200)
+async def like_story(
+        story_id: int,
+        session: SessionData = Depends(verifier)
 ):
-    user_id = session.get("user_id")
-    if not user_id:
-        raise HTTPException(status_code=401, detail="Not authorized")
+    user = User.get_by_id(session, session.user_id)
 
-    # проверяем, не поставил ли пользователь уже лайк или отменил его
-    like_status = get_like_status(db, item_id, user_id)
-    if like_status == "liked":
-        raise HTTPException(status_code=400, detail="User has already liked the item")
-    elif like_status == "unliked":
-        raise HTTPException(status_code=400, detail="User has already unliked the item")
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found.")
 
-    try:
-        add_like(db, item_id, user_id)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    return {"message": f"Item {item_id} was liked"}
+    story = Story.get_by_id(session, story_id)
 
+    if not story:
+        raise HTTPException(status_code=404, detail="Story not found.")
 
-@router.delete("/likes/{item_id}")
-async def unlike_item(
-        item_id: int,
-        session: FastAPISession = Depends(),
-        db: Session = Depends(get_session),
-):
-    user_id = session.get("user_id")
-    if not user_id:
-        raise HTTPException(status_code=401, detail="Not authorized")
+    if story in user.liked_stories:
+        user.liked_stories.remove(story)
+    else:
+        user.liked_stories.append(story)
 
-    # проверяем, не отменил ли пользователь уже лайк
-    like_status = get_like_status(db, item_id, user_id)
-    if like_status == "unliked":
-        raise HTTPException(status_code=400, detail="User has already unliked the item")
-    elif like_status == "not_liked":
-        raise HTTPException(status_code=400, detail="User has not liked the item yet")
+    session.add(user)
+    session.commit()
+    session.refresh(user)
 
-    try:
-        remove_like(db, item_id, user_id)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    return {"message": f"Like for item {item_id} was removed"}
+    return {"status": "success", "liked_stories": [s.id for s in user.liked_stories]}
