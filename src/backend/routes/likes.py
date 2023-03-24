0ad73bdd61a681a2604ebd9c5aa5961ee560acb9
Nikita Davidchuk <bizy18588@gmail.com>
2023-03-24 22:58:55 +0300

    review 1

--- src/backend/routes/likes.py
+++ src/backend/routes/likes.py
@@ -1,33 +1,57 @@
 from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session
 
-from src.backend.database.orm import User
+from src.backend.database import engine
+from src.backend.database.orm import User, Story
 from src.backend.dependencies import cookie, verifier
 from src.backend.sessions import SessionData
 
 app = APIRouter()
 
-@app.post('/like-story/{story_id}', dependencies=[Depends(cookie)], status_code=200)
+
+@app.post('/story/like/{story_id}', dependencies=[Depends(cookie)], status_code=200)
 async def like_story(
         story_id: int,
         session: SessionData = Depends(verifier)
 ):
-    user = User.get_by_id(session, session.user_id)
+    with Session(engine) as transaction:
+        user = User.get_by_id(transaction, session.user_id)
 
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found.")
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found.")
 
-    story = Story.get_by_id(session, story_id)
+        story = Story.get_by_id(transaction, story_id)
 
-    if not story:
-        raise HTTPException(status_code=404, detail="Story not found.")
+        if not story:
+            raise HTTPException(status_code=404, detail="Story not found.")
 
-    if story in user.liked_stories:
-        user.liked_stories.remove(story)
-    else:
-        user.liked_stories.append(story)
+        if story in user.liked_stories:
+            raise HTTPException(status_code=409, detail="Story already liked.")
 
-    session.add(user)
-    session.commit()
-    session.refresh(user)
+        user.like_story(transaction, story_id)
 
-    return {"status": "success", "liked_stories": [s.id for s in user.liked_stories]}
+        return {"status": True}
+
+
+@app.post('/story/dislike/{story_id}', dependencies=[Depends(cookie)], status_code=200)
+async def dislike_story(
+        story_id: int,
+        session: SessionData = Depends(verifier)
+):
+    with Session(engine) as transaction:
+        user = User.get_by_id(transaction, session.user_id)
+
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found.")
+
+        story = Story.get_by_id(transaction, story_id)
+
+        if not story:
+            raise HTTPException(status_code=404, detail="Story not found.")
+
+        if story not in user.liked_stories:
+            raise HTTPException(status_code=409, detail="Story already liked.")
+
+        user.dislike_story(transaction, story_id)
+
+        return {"status": True}
