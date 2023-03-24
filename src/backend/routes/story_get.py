7d3bd8b75a553c160a0ac38e11809f325ad2dbe1
Nikita Davidchuk <bizy18588@gmail.com>
2023-03-25 00:13:08 +0300

    review 2

--- src/backend/routes/story_get.py
+++ src/backend/routes/story_get.py
@@ -4,13 +4,13 @@ from sqlmodel import Session
 from src.backend.database import engine
 from src.backend.database.orm import Story
 from src.backend.dependencies import cookie, verifier
+from src.backend.internals.stories import StoryResponse
 from src.backend.sessions import SessionData
 
 app = APIRouter()
 
-@app.get("/story", dependencies=[Depends(cookie)])
+
+@app.get("/story", response_model=list(StoryResponse), dependencies=[Depends(cookie)])
 def get_some_stories(limit: int, offset: int, _: SessionData = Depends(verifier)):
     with Session(engine) as transaction:
-        stories = Story.get_all(transaction)
-    # limit - количество историй, offset - сдвиг
-    return stories[offset:][:limit]
+        return Story.get_all(transaction, limit, offset)
