6f4eb50f9f950011c47aa5ab0d108806ca7a3558
Nikita Davidchuk <bizy18588@gmail.com>
2023-03-25 00:42:44 +0300

    review 1

--- src/backend/routes/get_all_stories_by_userID.py
+++ src/backend/routes/get_all_stories_by_userID.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from src.backend.dependencies import cookie, verifier
+from src.backend.internals.goals import GoalResponse
 from src.backend.sessions import SessionData
 from src.backend.database.orm import Story
 
@@ -10,14 +11,14 @@ from src.backend.database import engine
 app = APIRouter()
 
 
-@app.get("/get_story")
-def get_by_user_id():
-    pass
+@app.get("/story/{user_id}", response_model=list(GoalResponse), dependencies=[Depends(cookie)])
+def get_story_by_id(user_id: int, limit: int = 100, offset: int = 0, _: SessionData = Depends(verifier)):
+    if limit > 100:
+        limit = 100
 
-
-@app.get("/get_story", dependencies=[Depends(cookie)])
-def get_story_by_id(user_id: int, session: SessionData = Depends(verifier)) -> Story:
     with Session(engine) as transaction:
-        story = Story.get_by_user_id(transaction, user_id)
+        stories = Story.get_by_user_id(transaction, user_id, limit, offset)
 
-    return story
+        stories = [story for story in stories if not story.deleted]
+
+        return stories
