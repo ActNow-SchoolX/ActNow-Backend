eb2f3a5265c3bdcd587c66c61df4d50749f6e191
Nikita Davidchuk <bizy18588@gmail.com>
2023-03-25 00:31:17 +0300

    review 1

--- src/backend/routes/goal_update_endpoint.py
+++ src/backend/routes/goal_update_endpoint.py
@@ -1,17 +1,34 @@
 from fastapi import HTTPException, Depends, APIRouter
-from fastapi.openapi.models import Response
+from sqlmodel import Session
 
+from src.backend.database import engine
+from src.backend.database.orm import Goal
 from src.backend.dependencies import verifier, cookie
-from src.backend.internals.goals import GoalResponse
+from src.backend.internals.goals import GoalPatchRequest, GoalResponse
 from src.backend.sessions import SessionData
 
 app = APIRouter()
 
 
-@app.patch("/goal/{goal_id}", response_model=Response, dependencies=[Depends(cookie)], status_code=201)
-async def update_goal(item: GoalResponse, session: SessionData = Depends(verifier)):
-    updated_goal = update_goal(item, session.user_id)
-    if updated_goal is None:
-        raise HTTPException(status_code=404, detail="Not found")
+@app.patch("/goal/{goal_id}", response_model=GoalResponse, dependencies=[Depends(cookie)], status_code=200)
+async def update_goal(goal_id: int, item: GoalPatchRequest, session: SessionData = Depends(verifier)):
+    with Session(engine) as transaction:
+        goal = Goal.get_by_id(transaction, goal_id)
 
-    return updated_goal
+        if goal is None:
+            raise HTTPException(status_code=404, detail="Goal with specified id is not found.")
+
+        if goal.deleted:
+            raise HTTPException(status_code=404, detail="Goal with specified id is not found.")
+
+        if goal.user_id != session.user_id:
+            raise HTTPException(status_code=403, detail="Forbidden")
+
+        goal.title = item.title
+        goal.description = item.description
+        goal.price = item.price
+        goal.deadline = item.deadline
+
+        goal.update(transaction)
+
+        return goal
